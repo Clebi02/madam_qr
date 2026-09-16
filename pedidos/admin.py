b2b185c5a8ee04563c37aad824061695651bdb6f
Clebi02 <cramosramosra@ucvvirtual.edu.pe>
2026-09-16 13:15:47 -0500

    Mostrar token de mesa en admin

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Categoria, Mesa, Plato, Pedido, DetallePedido
 
+@admin.register(Mesa)
+class MesaAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'token')
+    readonly_fields = ('token',)
+
 admin.site.register(Categoria)
-admin.site.register(Mesa)
 admin.site.register(Plato)
 admin.site.register(Pedido)
 admin.site.register(DetallePedido)
